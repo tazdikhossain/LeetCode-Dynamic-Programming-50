f92390b5e70c1f1902946255d99fdaec982b7d3a
TAZDIK HOSSAIN <tazdik755@gmail.com>
2025-05-24 16:38:09 +0600

    Fibonacci Number.cpp

---
+++ Fibonacci Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fib(int n) {
+        
+        if(n==0) return 0;
+        if(n==1) return 1;
+
+        int a = 0, b=1, result;
+
+        for(int i=2; i<=n; ++i)
+        {
+            result = a+b;
+            a=b;
+            b=result;
+        }
+
+        return b;
+    }
+};
