234cdef7a8baaf6c46c652d379422609d6b2c007
TAZDIK HOSSAIN <tazdik755@gmail.com>
2025-06-16 17:03:21 +0600

    Maximal Square.cpp

---
+++ Maximal Square.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) 
+    {
+        if (matrix.empty() || matrix[0].empty()) return 0;
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int maxSide = 0;
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; ++i) 
+        {
+            for (int j = 0; j < n; ++j) 
+            {
+                if (matrix[i][j] == '1') 
+                {
+                    if (i == 0 || j == 0) 
+                    {
+                        dp[i][j] = 1;
+                    }
+                    else 
+                    {
+                        dp[i][j] = min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]}) + 1;
+                    }
+                    maxSide = max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        return maxSide * maxSide;
+    }
+};
